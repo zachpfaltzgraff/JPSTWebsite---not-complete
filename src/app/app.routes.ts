ea778437132fd3dbf284d674838b3eea7a92b444
zachpfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2023-12-13 14:30:58 -0500

    Login / signup page
    created the visuals of the login / signup pages. Going to need to do the password confirmation next

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 export const routes: Routes = [
-    {path: 'login', component: LoginPageComponent}
+    {path: 'login', component: LoginPageComponent},
+    {path: 'signup', component: SignupPageComponent}
 ];
