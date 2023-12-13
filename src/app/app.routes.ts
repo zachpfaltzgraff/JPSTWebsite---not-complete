8dc862567422a830fb19a1fdc2189f0683dd35d0
zachpfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2023-12-13 13:36:51 -0500

    image
    added background image for the login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 
 export const routes: Routes = [
-    {path: 'login-page', component: LoginPageComponent}
+    {path: 'login', component: LoginPageComponent}
 ];
