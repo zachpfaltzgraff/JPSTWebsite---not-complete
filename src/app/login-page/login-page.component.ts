2d4f914b24ebee4b3aa0420aeca2d6379132d688
zachpfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2023-12-19 15:58:16 -0500

    FormControls / Groups
    Added in the storing of the email and password into the ts. going to worry about the backend of that later.
    Also added in so that the password and confirm password must be the same

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, MinLengthValidator, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
   onSubmit() {
-  throw new Error('Method not implemented.');
+    const emailValue = this.profileForm.value.email ?? '';
+    const passwordValue = this.profileForm.value.password ?? '';
+
+    console.log('Email:', emailValue);
+    console.log('Password:', passwordValue);
   }
-  
+
   profileForm = new FormGroup( {
     email: new FormControl(''),
     password: new FormControl(''),
-  });
+  })
 }
--- src/app/signup-page/signup-page.component.ts
+++ src/app/signup-page/signup-page.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.css'
 })
 export class SignupPageComponent {
+  onSubmit() {
+    const emailValue = this.profileForm.value.email ?? '';
+    const passwordValue = this.profileForm.value.password ?? '';
+    const confirmPasswordValue = this.profileForm.value.confirmPassword ?? '';
 
+    if (passwordValue === confirmPasswordValue) {
+      console.log('Email:', emailValue);
+      console.log('Password:', passwordValue);
+    }
+    else {
+      alert('Error: Passwords Must Match');
+      window.location.reload();
+    }
+  }
+
+  profileForm = new FormGroup( {
+    email: new FormControl(''),
+    password: new FormControl('', Validators.minLength(8)),
+    confirmPassword: new FormControl('')
+  });
 }
