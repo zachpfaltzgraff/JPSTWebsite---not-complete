a9eee4fdc20193ac60b678fe1815846d49398d51
zachpfaltzgraff <zachpfaltzgraff@gmail.com>
2023-12-22 09:54:19 -0500

    Created header bar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginPageComponent},
-    {path: 'signup', component: SignupPageComponent}
+    {path: 'signup', component: SignupPageComponent},
+    {path: 'header', component: HeaderBarComponent}
 ];
---
+++ src/app/header-bar/header-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-bar.component.html',
+  styleUrl: './header-bar.component.css'
+})
+export class HeaderBarComponent {
+
+}
