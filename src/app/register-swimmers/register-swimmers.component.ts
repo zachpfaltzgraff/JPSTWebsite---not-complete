ae95263dccbc8ba88bfec40f25b35b8fbed39ad9
zachpfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2024-01-02 19:57:28 -0500

    Added cancel button
    added cancel button as well as started some of the css. Have to fix it though since it doesnt think a valid form is actually valid

--- src/app/register-swimmers/register-swimmers.component.ts
+++ src/app/register-swimmers/register-swimmers.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register-swimmers',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register-swimmers.component.html',
   styleUrl: './register-swimmers.component.css'
 })
 export class RegisterSwimmersComponent {
+  onSubmit() {
+    if (this.profileForm.valid) {
+      const firstName = this.profileForm.value.firstName ?? '';
+      const lastName = this.profileForm.value.lastName ?? '';
+      const prefferedName = this.profileForm.value.prefferedName ?? firstName;
+      const birthDate = this.profileForm.value.birthDate ?? '';
 
+      const pFirstName = this.profileForm.value.pFirstName ?? '';
+      const pLastName = this.profileForm.value.pLastName ?? '';
+      const pPhoneNumber = this.profileForm.value.pPhoneNumber ?? '';
+      const pAddress = this.profileForm.value.pAddress ?? '';
+
+      const eFirstName = this.profileForm.value.eFirstName ?? '';
+      const eLastName = this.profileForm.value.eLastName ?? '';
+      const ePhoneNumber = this.profileForm.value.ePhoneNumber ?? '';
+      const eAddress = this.profileForm.value.eAddress ?? '';
+    }
+    else {
+      alert("Please fill in all required fields");
+    }
+  }
+
+
+
+
+  profileForm = new FormGroup( {
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    prefferedName: new FormControl(''),
+    birthDate: new FormControl('', Validators.required),
+    pFirstName: new FormControl('', Validators.required),
+    pLastName: new FormControl('', Validators.required),
+    pPhoneNumber: new FormControl('', Validators.required),
+    pAddress: new FormControl('', Validators.required),
+    eFirstName: new FormControl('', Validators.required),
+    eLastName: new FormControl('', Validators.required),
+    ePhoneNumber: new FormControl('', Validators.required),
+    eAddress: new FormControl('', Validators.required)
+  })
 }
