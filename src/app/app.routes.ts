eec03af849b178033cf6afda135424bb225c4142
zachpfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2024-01-11 17:04:10 -0500

    Created home page
    created the home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 import { RegisterSwimmersComponent } from './register-swimmers/register-swimmers.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginPageComponent},
     {path: 'signup', component: SignupPageComponent},
-    {path: 'register-swimmers', component: RegisterSwimmersComponent}
+    {path: 'register-swimmers', component: RegisterSwimmersComponent},
+    {path: '', component: HomePageComponent}
 ];
