2279e285c278dbdfe490dc19df4b4eb86d19d730
zachpfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2024-01-11 19:17:00 -0500

    contact us form
    contact us form was started

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 import { RegisterSwimmersComponent } from './register-swimmers/register-swimmers.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginPageComponent},
     {path: 'signup', component: SignupPageComponent},
     {path: 'register-swimmers', component: RegisterSwimmersComponent},
-    {path: '', component: HomePageComponent}
+    {path: '', component: HomePageComponent},
+    {path: 'contact', component: ContactComponent}
 ];
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
+  onSubmit() {
+    if(this.contactForm.valid) {
+      const firstName = this.contactForm.value.firstName ?? '';
+      const lastName = this.contactForm.value.lastName ?? '';
+      const email = this.contactForm.value.email ?? '';
+      const message = this.contactForm.value.message ?? '';
+    }
+    else {
+      alert('Please Fill in all fields');
+    }
+  }
+
+
+  contactForm = new FormGroup( {
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    message: new FormControl('', Validators.required)
+  })
 }
