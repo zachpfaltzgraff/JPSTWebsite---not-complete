9c8dab1d7e2ca724cc492660793d161ca768bda0
zachpfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2024-01-23 19:51:37 -0500

    created verify page

---
+++ src/app/verify-login/verify-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './verify-login.component.html',
+  styleUrl: './verify-login.component.css'
+})
+export class VerifyLoginComponent {
+
+}
