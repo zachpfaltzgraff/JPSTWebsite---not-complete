22ae248bd9e0ce9f5ec060265765af73d18531d7
zachpfaltzgraff <zachpfaltzgraff@gmail.com>
2024-01-30 12:50:36 -0500

    changed look lol

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,19 +6,14 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { ContactComponent } from './contact/contact.component';
 import { VerifyLoginComponent } from './verify-login/verify-login.component';
 
-import { 
-    AuthGuardService as AuthGuard 
-  } from '../auth-guard.service';
+import { AuthGuardService as AuthGuard } from '../auth-guard.service';
 import { inject } from '@angular/core';
 
 export const routes: Routes = [
     {path: 'login', component: LoginPageComponent},
     {path: 'signup', component: SignupPageComponent},
-    {
-        path: 'register-swimmers',
-        component: RegisterSwimmersComponent,
-        canActivate: [() => inject(AuthGuard).canActivate()]
-    },
+    { path: 'register-swimmers', component: RegisterSwimmersComponent, 
+        canActivate: [() => inject(AuthGuard).canActivate()]},
     {path: '', component: HomePageComponent},
     {path: 'contact', component: ContactComponent},
     {path: 'verify', component: VerifyLoginComponent},
