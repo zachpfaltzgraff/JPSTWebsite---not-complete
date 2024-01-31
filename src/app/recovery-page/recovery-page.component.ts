d375fe1e3e54f99324f776e9d83d09357ad11607
zachpfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2024-01-31 18:50:43 -0500

    changed format

--- src/app/recovery-page/recovery-page.component.ts
+++ src/app/recovery-page/recovery-page.component.ts
@@ -13,6 +13,7 @@ import {confirmResetPassword, type ConfirmResetPasswordInput} from 'aws-amplify/
 })
 export class RecoveryPageComponent {
 
+
   resetPassword() {
     const email = this.profileForm.value.email ?? '';
     handleResetPassword(email);
@@ -45,7 +46,6 @@ function handleResetPasswordNextSteps(output: ResetPasswordOutput) {
       console.log(
         `Confirmation code was sent to ${codeDeliveryDetails.deliveryMedium}`
       );
-      // Collect the confirmation code from the user and pass to confirmResetPassword.
       break;
     case 'DONE':
       console.log('Successfully reset password.');
