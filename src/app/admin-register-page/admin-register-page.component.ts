00d45f1e67f92e9a5ff394a42a063e799bcd4971
zachpfaltzgraff <zachpfaltzgraff@gmail.com>
2024-03-16 15:02:29 -0400

    get is working

--- src/app/admin-register-page/admin-register-page.component.ts
+++ src/app/admin-register-page/admin-register-page.component.ts
@@ -1,12 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import cdkOutput from '../../../../jpstCDK/output.json';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-register-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-register-page.component.html',
   styleUrl: './admin-register-page.component.css'
 })
 export class AdminRegisterPageComponent {
+  constructor(private http: HttpClient) {};
 
+  apiEndpoint = cdkOutput.LambdaStack.APIEndpoint1793E782;
+  userData: any;
+
+  ngOnInit(): void {
+    this.http.get<any>(this.apiEndpoint + 'swimmer/scan-data-swimmer')
+    .pipe(catchError(error => {
+      console.error('Error: ', error);
+      return throwError(error);
+    }))
+    .subscribe(response => {
+      this.userData = response.data;
+      console.log('Retrieved user data:', this.userData);
+    });
+  }
 }
